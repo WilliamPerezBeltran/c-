6e2d3cc631183e3b1b36595aee7f097f0c10a3ae
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-03-06 16:57:49 -0500

    deletion

--- lecture-01.c
+++ lecture-01.c
@@ -1,19 +1,69 @@
 #include <stdio.h>
-main() {
-int LA[] = {1,3,5,7,8};
-int item = 10, k = 3, n = 5;
-int i = 0, j = n;
-printf("The original array elements are :\n");
-for(i = 0; i<n; i++) {
-printf("LA[%d] = %d \n", i, LA[i]);
-}n = n + 1;
-while( j >= k) {
-LA[j+1] = LA[j];
-j = j - 1;
-}
-LA[k] = item;
-printf("The array elements after insertion :\n");
-for(i = 0; i<n; i++) {
-printf("LA[%d] = %d \n", i, LA[i]);
-}
+int main() {
+
+
+	void insertion(){
+	
+		int array[10] = {1,3,5,7,9};
+	int item = 10;
+	int n = 5, k = 3;
+	int i = 0, j = n;
+
+	printf("Old array: \n");
+	for (i = 0; i<n ; i++){
+		printf("array[%d] => %d \n", i, array[i]);
+	}
+
+	while(j >= k){
+		array[j+1] = array[j];
+		j = j - 1;
+
+	}
+	array[k] = item; 
+	n = n+1;
+	printf("New array: \n");
+	for (i = 0; i<n ; i++){
+		printf("array[%d] => %d \n", i, array[i]);
+	}
+
+	}
+
+	void deletion(){
+		int array[10] = {1,3,5,7,9};
+		int k = 2, n = 5;
+		int i,j;
+
+		printf("Old array: \n");
+		for(i = 0;i<n;i++){
+			printf("array[%d] => %d \n",i,array[i]);
+		}
+		
+		j = k;
+		while(j < n ){
+			array[j] = array[j+1];
+			j = j+1;
+
+		}
+		printf("New array: \n");
+		n = n-1;
+		for(i = 0;i<n;i++){
+			printf("array[%d] => %d \n",i,array[i]);
+		}
+		
+
+		
+	}
+
+
+
+
+	//insertion();
+	deletion();
+
+
+
+
+
+
+	return 0;
 }
